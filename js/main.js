4c41002465ab2c3c30804387469ae9ad650a9adb
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-06-05 23:37:45 +0600

    done

--- js/main.js
+++ js/main.js
@@ -37,6 +37,15 @@ window.onload = () =>{
     }else{
         document.querySelector('.header .header-2').classList.remove('active');
     }
+    fadeOut();
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+  setTimeout(loader,4000);
 }
 
 
