95ce23191a5f7b421497795e120d5f9a3f2a69c5
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-06-05 22:34:58 +0600

    blog  section  add

--- js/main.js
+++ js/main.js
@@ -154,3 +154,32 @@ var swiper = new Swiper(".featured-slider", {
       },
     },
   });
+
+
+  var swiper = new Swiper(".blog-slider", {
+    spaceBetween:10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+    
+      },
+     
+      768: {
+        slidesPerView: 2,
+       
+      },
+      1024: {
+        slidesPerView: 3,
+       
+      },
+    },
+  });
